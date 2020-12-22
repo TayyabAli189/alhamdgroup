d4a39322a310e3dad0f08d9a167b6f262ec25f1c
tayyabali <tayyabalit189@gmail.com>
2020-12-22 16:21:07 +0500

    first commit

---
+++ resources/views/include/scripts.blade.php
@@ -0,0 +1,23 @@
+<!-- Javascript Files
+    ================================================== -->
+
+<!-- initialize jQuery Library -->
+<script src="{{asset('frontend/plugins/jQuery/jquery.min.js')}}"></script>
+<!-- Bootstrap jQuery -->
+<script src="{{asset('frontend/plugins/bootstrap/bootstrap.min.js')}}" defer></script>
+<!-- Slick Carousel -->
+<script src="{{asset('frontend/plugins/slick/slick.min.js')}}"></script>
+<script src="{{asset('frontend/plugins/slick/slick-animation.min.js')}}"></script>
+<!-- Color box -->
+<script src="{{asset('frontend/plugins/colorbox/jquery.colorbox.js')}}"></script>
+<!-- shuffle -->
+<script src="{{asset('frontend/plugins/shuffle/shuffle.min.js')}}" defer></script>
+
+
+<!-- Google Map API Key-->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU" defer></script>
+<!-- Google Map Plugin-->
+<script src="{{asset('frontend/plugins/google-map/map.js')}}" defer></script>
+
+<!-- Template custom -->
+<script src="{{asset('frontend/js/script.js')}}"></script>
---
+++ resources/views/include/styles.blade.php
@@ -0,0 +1,16 @@
+
+<!-- CSS
+================================================== -->
+<!-- Bootstrap -->
+<link rel="stylesheet" href="{{asset('frontend/plugins/bootstrap/bootstrap.min.css')}}">
+<!-- FontAwesome -->
+<link rel="stylesheet" href="{{asset('frontend/plugins/fontawesome/css/all.min.css')}}">
+<!-- Animation -->
+<link rel="stylesheet" href="{{asset('frontend/plugins/animate-css/animate.css')}}">
+<!-- slick Carousel -->
+<link rel="stylesheet" href="{{asset('frontend/plugins/slick/slick.css')}}">
+<link rel="stylesheet" href="{{asset('frontend/plugins/slick/slick-theme.css')}}">
+<!-- Colorbox -->
+<link rel="stylesheet" href="{{asset('frontend/plugins/colorbox/colorbox.css')}}">
+<!-- Template styles-->
+<link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
